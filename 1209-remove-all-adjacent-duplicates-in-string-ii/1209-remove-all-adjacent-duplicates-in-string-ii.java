c4c941e8e791ae5191e501d75f6fe0ce5e84ebc1
abhijay rane <94125959+abjR265@users.noreply.github.com>
2024-11-04 19:16:42 -0500

    Time: 15 ms (90.21%), Space: 45.7 MB (41.17%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,28 +1,23 @@
 class Solution {
    public String removeDuplicates(String s, int k) {
-    // Use a StringBuilder to store characters and an integer stack to track counts.
     StringBuilder sb = new StringBuilder();
     int[] count = new int[s.length()]; // Array to track counts of characters
 
     for (char c : s.toCharArray()) {
-        sb.append(c); // Add the current character to StringBuilder
+        sb.append(c);
         int size = sb.length();
+        
+        count[size - 1] = (size > 1 && sb.charAt(size - 1) == sb.charAt(size - 2)) 
+                          ? count[size - 2] + 1 
+                          : 1;
 
-        if (size > 1 && sb.charAt(size - 1) == sb.charAt(size - 2)) {
-            // If the current character is the same as the previous character, increment the count
-            count[size - 1] = count[size - 2] + 1;
-        } else {
-            // Otherwise, start a new count for this character
-            count[size - 1] = 1;
-        }
-
-        // If the count reaches k, remove the last k characters from StringBuilder
         if (count[size - 1] == k) {
-            sb.delete(sb.length() - k, sb.length());
+            sb.delete(size - k, size);
         }
     }
 
-    return sb.toString(); // Return the final string
+    return sb.toString();
 }
+
 
 }
