f6520c2698450864b81247db00937ad5f537928c
abhijay rane <94125959+abjR265@users.noreply.github.com>
2024-11-05 00:15:37 -0500

    Time: 14 ms (92.30%), Space: 45.6 MB (47.43%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -7,9 +7,7 @@ class Solution {
         sb.append(c);
         int size = sb.length();
         
-        count[size - 1] = (size > 1 && sb.charAt(size - 1) == sb.charAt(size - 2)) 
-                          ? count[size - 2] + 1 
-                          : 1;
+        count[size - 1] = (size > 1 && sb.charAt(size - 1) == sb.charAt(size - 2)) ? count[size - 2] + 1 : 1;
 
         if (count[size - 1] == k) {
             sb.delete(size - k, size);
