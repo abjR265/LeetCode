8dd27637dc351b9e529ca2dc8520bb8dc705c2ba
abhijay rane <94125959+abjR265@users.noreply.github.com>
2024-11-10 23:16:54 -0500

    Time: 3 ms (44.77%), Space: 41.1 MB (97.44%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,29 @@
 class Solution {
+    public HashMap<Character, Character> map;
+    public Solution(){
+        this.map = new HashMap<Character, Character>();
+        this.map.put(']','[');
+        this.map.put(')','(');
+        this.map.put('}','{');
+    }
     public boolean isValid(String s) {
-        // Create hashmap to store the pairs...
-        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
-        Hmap.put(')','(');
-        Hmap.put('}','{');
-        Hmap.put(']','[');
-        // Create stack data structure...
-        Stack<Character> stack = new Stack<Character>();
-        // Traverse each charater in input string...
-        for (int idx = 0; idx < s.length(); idx++){
-            // If open parentheses are present, push it to stack...
-            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
-                stack.push(s.charAt(idx));
-                continue;
+        Stack <Character> stk = new Stack<>();
+        for (int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            
+            
+            if (this.map.containsKey(c)){
+                char top = stk.empty() ? '0' : stk.pop();
+                
+                if (top!=map.get(c)){
+                    return false;
+                } }
+            else {
+                    stk.push(c);
+                }
             }
-            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
-            // If not, we need to return false...
-            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
-                return false;
-            }
-        }
-        // If the stack is empty, return true...
-        if (stack.size() == 0) {
-            return true;
-        }
-        return false;
+         return stk.isEmpty();
     }
 }
+        
+    
